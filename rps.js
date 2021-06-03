02d5fdd68d82475cb50ccf226175eb0f79479dcf
Dzole <dzolekirovski@gmail.com>
2021-06-03 21:57:01 +0200

    Created buttons and div output. JS edited for clicking buttons

--- rps.js
+++ rps.js
@@ -1,12 +1,22 @@
-function userInput(){
-    let userInput;
-    do{
-        userInput = prompt("Rock, Paper, Scissors? Choose one of the three moves");
-        userInput = userInput.slice(0,1).toUpperCase().concat(userInput.slice(1).toLowerCase());
-    }while(userInput !== 'Rock' && userInput !=='Paper' && userInput !== 'Scissors')
+const buttons = document.querySelectorAll('div#wrapper > button');
+buttons.forEach(button => {
+    button.addEventListener('click', e => {
+        console.log(e.target.innerText)
+        playRound(e.target.innerText, computerPlay());
+    })
+})
 
-    return userInput;
-}
+
+
+// function userInput(){
+//     let userInput;
+//     do{
+//         userInput = prompt("Rock, Paper, Scissors? Choose one of the three moves");
+//         userInput = userInput.slice(0,1).toUpperCase().concat(userInput.slice(1).toLowerCase());
+//     }while(userInput !== 'Rock' && userInput !=='Paper' && userInput !== 'Scissors')
+
+//     return userInput;
+// }
 
 function computerPlay(){
     let play = ['Rock', 'Paper', 'Scissors'];
@@ -14,8 +24,10 @@ function computerPlay(){
 }
 
 function playRound(playerSelection, computerSelection){
+    let resultPara = document.createElement('p');
     if(playerSelection === computerSelection){
         console.log(`Draw! Player: ${playerSelection}, Computer: ${computerSelection}`);
+        resultPara.textContent = `Draw! Player: ${playerSelection}, Computer: ${computerSelection}`;
         return 0;
     }else if(
         (playerSelection === "Rock" && computerSelection === "Paper") ||
@@ -30,29 +42,30 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-function game(){
-    let userScore = 0;
-    let computerScore = 0;
-    for(let i = 1; i <= 5; i++){
-        let match = playRound(userInput(),computerPlay());
-        if (match===-1){
-            computerScore++;
-        }
-        else if(match===1){
-            userScore++;
-        }
-    }
-    console.log(`Scoreboard:`);
-    console.log(`User: ${userScore}, Computer: ${computerScore}`);
-    if(!(userScore-computerScore)){
-        return 'Draw!'
-    }
-    else if(userScore-computerScore>0){
-        return 'You Win!';
-    }else{
-        return 'You Lose!'
-    }
-}
 
-console.log("Best of five!");
-console.log(game());
+// console.log("Play game!");
+// playRound(userInput(), computerPlay()); 
+
+// function game(){
+//     let userScore = 0;
+//     let computerScore = 0;
+//     for(let i = 1; i <= 5; i++){
+//         let match = playRound(userInput(),computerPlay());
+//         if (match===-1){
+//             computerScore++;
+//         }
+//         else if(match===1){
+//             userScore++;
+//         }
+//     }
+//     console.log(`Scoreboard:`);
+//     console.log(`User: ${userScore}, Computer: ${computerScore}`);
+//     if(!(userScore-computerScore)){
+//         return 'Draw!'
+//     }
+//     else if(userScore-computerScore>0){
+//         return 'You Win!';
+//     }else{
+//         return 'You Lose!'
+//     }
+// }
